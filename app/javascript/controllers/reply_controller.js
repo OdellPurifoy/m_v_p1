b26a6b21cd3ca464b01284d914ae5df6052997ce
Odell Purifoy <odell@visitdays.com>
2020-10-20 19:10:52 -0400

    Base deployment configuration

--- app/javascript/controllers/reply_controller.js
+++ app/javascript/controllers/reply_controller.js
@@ -12,10 +12,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "form" ]
 
-  // connect() {
-  //   this.outputTarget.textContent = 'Hello, Stimulus!'
-  // }
-
   toggle(event) {
     event.preventDefault()
     this.formTarget.classList.toggle('d-none')
