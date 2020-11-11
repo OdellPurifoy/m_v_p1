0f316b69ec7dfb8168b7a26ff83302d35eaa6109
Odell Purifoy <odell@visitdays.com>
2020-11-10 20:12:34 -0500

    Updated schema to use Postgresql

--- db/migrate/20200916212932_create_posts.rb
+++ db/migrate/20200916212932_create_posts.rb
@@ -3,9 +3,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title, null: false, default: ''
+      t.text :title, null: false, default: ''
       t.text :content, null: false, default: ''
-      t.string :category, null: false, default: ''
+      t.text :category, null: false, default: ''
       t.belongs_to :user, foreign_key: true
 
       t.timestamps
